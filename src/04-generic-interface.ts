c1b1cd7071544a5a7a6cd7f09de6fe5e61e2d2e0
abdur-shobur <abdurshobur.developer@gmail.com>
2023-05-05 09:08:54 +0600

    gen pre defined

--- src/04-generic-interface.ts
+++ src/04-generic-interface.ts
@@ -10,3 +10,21 @@ const NewValueGenInterface: GenInterFace<NewGenInterface> = {
 	age: 234,
 	name: 'New Name',
 };
+
+// generic interface using predefined type
+interface GenPreIn<T> {
+	name: string;
+	out: T;
+}
+
+// or
+type newGenType = { name: 'sd'; age: number };
+
+// or GenPreIn:newGenType
+const NewValue: GenPreIn<{ name: 'sd'; age: number }> = {
+	name: 'New',
+	out: {
+		age: 324,
+		name: 'sd',
+	},
+};
